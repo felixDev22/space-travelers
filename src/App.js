473ec9aa6c7f9b5cb4df357684345e985c1400b5
felixDev22 <felixnzomo@gmail.com>
2023-01-31 18:30:56 +0300

    Import Routes , route to activate profile link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
