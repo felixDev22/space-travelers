3ca0e584686fa937f0628d66e2c29576b0e842ac
felixDev22 <felixnzomo@gmail.com>
2023-02-03 02:22:13 +0300

    Add function to display resevered rockets

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './Profile.css';
 
-const Profile = () => (
-  <div>My Profile</div>
-);
+const Profile = () => {
+  const rocketList = useSelector((state) => state.rockets.data);
 
+  return (
+    <div className="profile-container">
+      <div className="rockets-holder">
+        <h2>My Rockets</h2>
+        <ul className="show-rockets">
+          {rocketList.map((rocket) => {
+            if (rocket.reserved) {
+              return (
+                <li className="show-list" key={rocket.id}>
+                  {rocket.name}
+                </li>
+              );
+            }
+            return '';
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Profile;
